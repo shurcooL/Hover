9776f6e5688176d6f52d619b22e5a32445714f1e
Dmitri Shuralyov <shurcooL@gmail.com>
2016-10-11 15:48:53 -0700

    Set working directory to where assets live.
    On desktop, this allows running `Hover` binary from anywhere.
    The source package must still be available in GOPATH.

---
+++ assets.go
@@ -0,0 +1,32 @@
+// +build !js
+
+package main
+
+import (
+	"go/build"
+	"log"
+	"os"
+)
+
+// Set the working directory to the root of Hover package, so that its assets can be accessed.
+func init() {
+	// importPathToDir resolves the absolute path from importPath.
+	// There doesn't need to be a valid Go package inside that import path,
+	// but the directory must exist.
+	importPathToDir := func(importPath string) (string, error) {
+		p, err := build.Import(importPath, "", build.FindOnly)
+		if err != nil {
+			return "", err
+		}
+		return p.Dir, nil
+	}
+
+	dir, err := importPathToDir("github.com/shurcooL/Hover")
+	if err != nil {
+		log.Fatalln("Unable to find github.com/shurcooL/Hover package in your GOPATH, it's needed to load assets:", err)
+	}
+	err = os.Chdir(dir)
+	if err != nil {
+		log.Panicln("os.Chdir:", err)
+	}
+}
